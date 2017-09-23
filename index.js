f693302ecb00d712ea2af436288d603d4d7225fb
Alexander Golovanov <agolovanov@textpress.co>
2017-09-22 22:38:04 -0500

    Enchantment: Replace flow plugin with flow preset

--- index.js
+++ index.js
@@ -4,10 +4,10 @@ module.exports = function ( context, opts ) {
     return {
         presets: [
             [ require( "babel-preset-env" ), opts ],
-            require( "babel-preset-bluebird" )
+            require( "babel-preset-bluebird" ),
+            require( "babel-preset-flow" )
         ],
         plugins: [
-            require( "babel-plugin-transform-flow-strip-types" ),
             require( "babel-plugin-transform-object-rest-spread" ),
             require( "babel-plugin-transform-runtime" )
         ]
